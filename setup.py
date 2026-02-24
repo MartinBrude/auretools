79cf658c6002a1ad27956701e76af49753658dba
Martín Brude <mb@pep.security>
2026-02-24 12:54:35 +0100

    Fix setup

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ with open("DESCRIPTION.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='auretools',
-    version='0.1.0',
+    version='1.1.0',
     author='Ferchus',
     author_email='fer@chus.ai',
     description='A compilation of F utility functions',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url='https://github.com/institutohumai/humai_utils',
+    url='https://github.com/MartinBrude/auretools',
     project_urls = {
         "Bug Tracker": "https://github.com/institutohumai/humai_utils/issues"
     },
